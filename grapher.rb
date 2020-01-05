1b16a942bcaf00eb52180140a860027821827c06
misprit7 <xandernaumenko@gmail.com>
2020-01-05 00:00:00 -0800

    2020-01-05 00:00:00 -0800

--- grapher.rb
+++ grapher.rb
@@ -64,8 +64,13 @@ end
 
 str_commit_dates = commit_dates.map(&:to_s)
 
+i = 1
 commit_dates.each do |date|
   puts date
   File.open('random_list_of_dates', 'w') { |f| f << str_commit_dates.shuffle.first(12).join("\n") }
   `GIT_AUTHOR_NAME="misprit7" GIT_AUTHOR_EMAIL="xandernaumenko@gmail.com" GIT_AUTHOR_DATE="#{date}" GIT_COMMITTER_DATE="#{date}" git commit -am "#{date}"`
+  if i > 30
+    `git push`
+  end
+  i += 1
 end
